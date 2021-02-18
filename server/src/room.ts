fb213e05b017011b0475a7c2c1624912dab6dfba
Sean McFadden <smcfadden150@gmail.com>
2021-02-18 16:21:48 -0500

    Baseline Video Playing and Pausing Functionality Complete!

--- server/src/room.ts
+++ server/src/room.ts
@@ -1,3 +1,4 @@
+import { reduceEachTrailingCommentRange } from 'typescript';
 import ProgressionType = require('./ProgressionType');
 
 const webSocket = require('ws');
@@ -29,7 +30,7 @@ class Room {
     playYoutubeVideo(url : string)
     {
         this.wss.forEach((ws : any) => {
-        this.ws.send(JSON.stringify({ actionType: 'PlayYoutubeVideo', url: url }));
+            ws.send(JSON.stringify({ actionType: 'PlayYoutubeVideo', url: url }));
         });
     }
 
@@ -38,31 +39,18 @@ class Room {
     parseAction(action : any) {
         let actionType = action.actionType;
         console.log(`PARSING: ${actionType}`);
-        switch(actionType) {
-            case 'PauseVideo': {
-
-            }
-            case 'ResumeVideo': {
-
-            }
-            case 'PlayYoutubeVideo': {
-                console.log("YOOO WE ARE PLAYING A VIDEO!!");
-                if (!action.url) {
-                    return;
-                }
-                this.playYoutubeVideo(action.url);
-            }
-            case 'ToggleVideo': {
-                console.log("HEY WE ARE TOGGLING THE VIDEO!");
-                this.mediaState = !this.mediaState;
-                this.wss.forEach((ws : any) => {
-                    ws.send(JSON.stringify({ actionType: 'ToggleVideo', state: this.mediaState }));
-                });
-                
-            }
-            default: {
-                console.log("could not parse room action string!");
-            }
+        if (actionType === 'PlayYoutubeVideo') {
+            console.log("YOOO WE ARE PLAYING A VIDEO!!");
+            this.playYoutubeVideo(action.url);
+            return;
+        }
+        if (actionType === 'ToggleVideo') {
+            console.log(`HEY WE ARE TOGGLING THE VIDEO! ${actionType}`);
+            this.mediaState = !this.mediaState;
+            this.wss.forEach((ws : any) => {
+                ws.send(JSON.stringify({ actionType: 'ToggleVideo', state: this.mediaState }));
+            });
+            return;
         }
     }
 
