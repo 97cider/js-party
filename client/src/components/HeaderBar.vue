8999f521dcb637ce5ae80520676680aa0d6d49a1
smcfadden150 <smcfadden150@gmail.com>
2021-03-05 16:14:19 -0500

    Added Animations to Collapsible Header :art:

--- client/src/components/HeaderBar.vue
+++ client/src/components/HeaderBar.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="header">
     <div class="header-container">
-      <div v-show="!isMinimized" class="header-content">
-        <div class="header-item">
-          <img class="icon" alt="lt" src="public/svgs/icon-left.svg">
+      <transition name="pop-slide">
+        <div v-show="!isMinimized" class="header-content">
+          <div class="header-item">
+            <img class="icon" alt="lt" src="public/svgs/icon-left.svg">
+          </div>
+          <div class="header-item">
+            <img class="logo" alt="lantern-party" src="public/svgs/LaternPartyLogo.svg">
+          </div>
+          <div class="header-item">
+            <img class="icon-right" alt="lt" src="public/svgs/icon-right.svg">
+          </div>
+          <div class="header-item-right">
+            <div v-on:click="hideHeaderBar">
+              <img class="button-icon" alt="^" src="public/svgs/expand-icon.svg">
+            </div>
+          </div>
         </div>
-        <div class="header-item">
-          <img class="logo" alt="lantern-party" src="public/svgs/LaternPartyLogo.svg">
-        </div>
-        <div class="header-item">
-          <img class="icon-right" alt="lt" src="public/svgs/icon-right.svg">
-        </div>
-        <div class="header-item-right">
-          <button v-on:click="hideHeaderBar">V</button>
+      </transition>
+    </div>
+    <transition name="pop-slide">
+      <div v-show="isMinimized" class="minimized-header">
+        <div class="header-item-top-right">
+          <div class="header-expand-content" v-on:click="showHeaderBar">
+            <img class="button-icon inverted" alt="^" src="public/svgs/expand-icon.svg">
+          </div>
         </div>
       </div>
-    </div>
-    <div v-show="isMinimized" class="minimized-header">
-       <div class="header-item-top-right">
-        <button v-on:click="showHeaderBar"> ^ </button>
-       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -47,17 +55,14 @@ export default {
     .header-container {
         // background: rgb(48, 71, 94);
         position: relative;
-        background: url(../../public/svgs/header-bg.svg);
-        background-repeat: repeat;
-        width: 100vw;
         height: 60px;
-        box-shadow: inset 0 -2px 5px rgba(0,0,0,.33);
+        width: 100vw;
     }
 
     .header-item-right {
       margin-left: auto;
       margin-right: 20px;
-      margin-top: 20px;
+      margin-top: 15px;
     }
 
     .header-item-top-right {
@@ -68,12 +73,25 @@ export default {
 
     .header-content {
         display: flex;
-        height: 100%;
+        height: 60px;
+        position: absolute;
+        width: 100%;
+        background: url(../../public/svgs/header-bg.svg);
+        background-repeat: repeat;
+        box-shadow: inset 0 -2px 5px rgba(0,0,0,.33);
+    }
+
+    .header-expand-content {
+      background: rgb(240, 84, 84);
+      border-radius: 0px 0px 10px 10px;
+      padding: 10px 3px 0px 3px;
     }
 
     .minimized-header {
       display: flex;
-      height: 60px;
+      position: absolute;
+      top: 0;
+      width: 100%;
     }
 
     .logo {
