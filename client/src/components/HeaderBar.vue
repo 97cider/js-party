0f837256027b7d37152cdfa0c6e6958bb7a2a956
smcfadden150 <smcfadden150@gmail.com>
2021-03-05 11:33:37 -0500

    Added Collapsible Header

--- client/src/components/HeaderBar.vue
+++ client/src/components/HeaderBar.vue
@@ -1,31 +1,82 @@
 <template>
-  <div class="header-container">
-    <div class="header-content">
-      <img class="icon" alt="lt" src="public/svgs/icon-left.svg">
-      <img class="logo" alt="lantern-party" src="public/svgs/LaternPartyLogo.svg">
+  <div class="header">
+    <div class="header-container">
+      <div v-show="!isMinimized" class="header-content">
+        <div class="header-item">
+          <img class="icon" alt="lt" src="public/svgs/icon-left.svg">
+        </div>
+        <div class="header-item">
+          <img class="logo" alt="lantern-party" src="public/svgs/LaternPartyLogo.svg">
+        </div>
+        <div class="header-item-right">
+          <button v-on:click="hideHeaderBar">V</button>
+        </div>
+      </div>
+    </div>
+    <div v-show="isMinimized" class="minimized-header">
+       <div class="header-item-top-right">
+        <button v-on:click="showHeaderBar"> ^ </button>
+       </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data: function() {
+    return {
+      isMinimized: false,
+    }
+  },
+  methods: {
+    hideHeaderBar: function () {
+      this.isMinimized = true;
+    },
+    showHeaderBar: function () {
+      this.isMinimized = false;
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
     .header-container {
         background: rgb(48, 71, 94);
+        position: relative;
+
+        width: 100vw;
+        box-shadow: inset 0 -2px 5px rgba(0,0,0,.33);
+    }
+
+    .header-item-right {
+      margin-left: auto;
+      margin-right: 20px;
+      margin-top: 20px;
+    }
+
+    .header-item-top-right {
+      margin-left: auto;
+      margin-right: 20px;
+      margin-top: 0px;
     }
 
     .header-content {
         display: flex;
-        justify-content: cente;
+    }
+
+    .minimized-header {
+      display: flex;
+      height: 63.5px;
     }
 
     .logo {
-        max-height: 70%;
-        max-width: 30%;
-        margin: 10px 0px -5px 10px;
+        height: 35px;
+	      margin: 15px 0px 0px 0px;
+    }
+
+    .icon {
+      height: 60px;
+      width: 60px;
     }
 </style>
